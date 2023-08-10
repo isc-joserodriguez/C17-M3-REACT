bf199c8ba3a12c6b707ace271cd258b8b4f4edfc
Jose Rodriguez <j.rodriguez@blackstone.studio>
2023-08-09 23:02:58 -0700

    Inicio de sesión

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const clickHandler = async () => {
+    try {
+      const {
+        data: { data },
+      } = await axios.post('http://localhost:3002/v1/auth/login', {
+        correo: email,
+        password,
+      });
+      console.log(data);
+
+      setToken(data.token);
+      localStorage.setItem('token', data.token);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {token}
+      <label>Email</label>
+      <input type="email" value={email} onChange={onChangeEmail} />
+      <br />
+      <label>Password</label>
+      <input type="password" value={password} onChange={onChangePassword} />
+      <br />
+      <button type="button" onClick={clickHandler}>
+        Iniciar sesión
+      </button>
+    </>
   );
 }
 
