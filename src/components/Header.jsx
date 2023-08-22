4b966e84626b6404ee8f6691ea493de3255add2e
Jose Rodriguez <j.rodriguez@blackstone.studio>
2023-08-21 19:44:59 -0700

    carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,51 @@ import { VentaContext } from '../context/VentaContext';
 
 const Header = () => {
   const { carrito, eliminarProductoCarrito } = useContext(VentaContext);
+
+const obtenerProductoObjeto = () =>{
+  const productoObject = {};
+  carrito.forEach(producto =>{
+    if(productoObject[producto._id]){
+      productoObject[producto._id].push(producto);
+    }else{
+      productoObject[producto._id] = [producto];
+    }
+  })
+
+  return productoObject;
+}
+  const mapearCarrito = () => {
+
+    {}
+    return carrito.forEach(()=>{});
+    /* 
+    [
+    {
+        "_id": "64d4560f20a967da48a7280a",
+        "nombre": "Helado",
+        "sabor": "Pera",
+        "precio": 20,
+        "proveedor": {
+            "_id": "64d451919ab04eb87b3653df",
+            "nombre": "Mauricio",
+            "telefono": "1223456788",
+            "__v": 0
+        },
+        "__v": 0
+    },
+]
+    */
+  }
+
+  const mostrarCarrito = () => {
+    if(carrito.length){
+      return mapearCarrito()
+    }
+    return <h5>Carrito vacío</h5>;
+  }
+
+
+
   return (
     <ul>
       <li>
@@ -19,15 +64,11 @@ const Header = () => {
         <Link to="/checkout">CheckoutPage</Link>
       </li>
       <li>productos: {carrito.length}</li>
+      
       <ul>
-        {carrito.map((producto) => (
-          <li key={producto._id}>
-            {producto.nombre}{' '}
-            <button onClick={() => eliminarProductoCarrito(producto._id)}>
-              eliminar
-            </button>
-          </li>
-        ))}
+        {
+          mostrarCarrito()
+        }
       </ul>
     </ul>
   );
